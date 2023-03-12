b90289d223421e3cb338ce15f24e0ca81a20281d
dwnmf <56194792+dwnmf@users.noreply.github.com>
2023-03-12 22:13:38 +0300

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,26 @@
+const mineflayer = require('mineflayer');
+
+// Set up bot options
+const botOptions = {
+  host:  ‘95.216.92.67',
+  port: ‘25648,
+  username: 'Bot'
+};
+
+// Create bot instance
+const bot = mineflayer.createBot(botOptions);
+
+// Listen for chat events
+bot.on('chat', (username, message) => {
+  // Check if the message contains "tp to me"
+  if (message.toLowerCase().includes('tp to me')) {
+    // Get the player object for the user who wrote the message
+    const player = bot.players[username];
+
+    // Check if the player object exists
+    if (player) {
+      // Send the "/tpa" command to the player
+      bot.chat(`/tpa ${player.username}`);
+    }
+  }
+});
